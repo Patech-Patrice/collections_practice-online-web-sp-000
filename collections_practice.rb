9a69fbee91808ca342cc8d3d3f92fbc6b9e8ffe7
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-16 18:53:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,21 +15,16 @@ array = ["cat", "dogs", "Horses"]
 def sort_array_char_count(array)
   array.sort do | a, b |
     a.length <=> b.length
+  end
 end
-end
-
-
-
 
 def swap_elements(array)
   array[1], array[2] = array[2], array[1]
   array
 end
 
-
-
 def reverse_array(array)
-  
+  array.reverse
 end
 
 
