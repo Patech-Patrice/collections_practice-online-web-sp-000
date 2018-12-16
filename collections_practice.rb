f8ad403b93a296271cb5d19d6b198b48794dea46
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-12-16 18:05:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,10 +1,6 @@
-# Question 1
-  describe '#sort_array_asc' do
-    it 'should return an array sorted in ascending order' do
-      expect(sort_array_asc([25, 7, 1])).to eq([1,7,25])
-    end
-  end
+
   
  def sort_array_asc
-   array.sort
+   array.sort!
+  end
  
